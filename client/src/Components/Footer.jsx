eead02abb0d38aba9e6e891d3f9cfc6a8043ba39
Gabi <gabyszabo5@gmail.com>
2023-03-27 14:23:41 +0200

    added navbar and footer components

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+//importing react-icons library to import the Facebook, Twitter, and Instagram icons as components
+
 
 function Footer() {
-  return <div>Footer</div>;
+  return ( <footer>
+    <div className="social-icons">
+      <a href="https://www.facebook.com/"><FaFacebook /></a>
+      <a href="https://twitter.com/"><FaTwitter /></a>
+      <a href="https://www.instagram.com/"><FaInstagram /></a>
+    </div>
+    //copyrights and company name
+    <p>&copy; 2023 ReTaGa</p>
+  </footer>);
 }
 
 export default Footer;
--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Components/NavBar.css";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+  <nav>
+    <ul class=" navbar">
+        <li><a href="#">Home</a></li>
+        <li><a href="#artview">ArtView</a></li>
+        <li><a href="#search">Search</a></li>
+       <li><a href="#tourview">TourView</a></li>
+       <li><a href="#videotour">VideoTour</a></li>
+    </ul>
+
+</nav>
+  );
 }
 
 export default NavBar;
