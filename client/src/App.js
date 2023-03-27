912ae7a7b7b5da4f5cbe4fdf64f438f754a89344
Tatiana <tatiana.motterle@gmail.com>
2023-03-27 11:43:15 +0200

    external API added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+const API_Key = 'IagrCxtB';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [collection, setCollection] = useState();
+	const [error, setError] = useState('');
+
+	const handleClick = (e) => {
+		e.preventDefault();
+
+		getCollection();
+	};
+
+	const getCollection = async () => {
+		try {
+			let response = await fetch(
+				`https://www.rijksmuseum.nl/api/en/collection?key=${API_Key}&hasImage=true&p=10.000&ps=100`
+			);
+			if (response.ok) {
+				let data = await response.json();
+				setCollection(data);
+				console.log(data);
+			} else {
+				setError(`Error: ${response.status} ${response.statusText}`);
+			}
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
+	return (
+		<div className="App">
+			<button
+				type="button"
+				onClick={handleClick}
+				className="btn btn-primary btn-lg btn-block">
+				START VIRTUAL TOUR
+			</button>
+			{collection && (
+				<div>
+					<h3>{collection.artObjects[0].title}</h3>
+					<img alt="description" src={collection.artObjects[0].webImage.url} />
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
