eb580009a313418f7ba26f2d85458062b6435274
Rebecca Asuan-O'Brien <raaob78@gmail.com>
2023-03-27 12:11:56 +0200

    MORE REACT ROUTER UPDATE COMPONENTS

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
 import Search from "./Pages/Search";
-import TourView from "./Pages/TourView"
-import ArtView from "./Pages/ArtView"
-import './App.css';
+import TourView from "./Pages/TourView";
+import ArtView from "./Pages/ArtView";
+import NavBar from "./Components/NavBar";
+import Footer from "./Components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Nav>
-
-        </Nav>
+        <NavBar />
         <Search />
         <TourView />
         <ArtView />
+        <Footer />
       </BrowserRouter>
-      
     </div>
   );
 }
