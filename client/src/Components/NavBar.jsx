b914ba99aa8a86106d459ec7f9bce176ff9d1ec3
Gabi <gabyszabo5@gmail.com>
2023-03-27 14:55:30 +0200

    added home

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -5,11 +5,12 @@ function NavBar() {
   return (
   <nav>
     <ul class=" navbar">
-        <li><a href="#">Home</a></li>
-        <li><a href="#artview">ArtView</a></li>
-        <li><a href="#search">Search</a></li>
-       <li><a href="#tourview">TourView</a></li>
-       <li><a href="#videotour">VideoTour</a></li>
+{/* the website can be navigated using only the keyboard with the tabIndex*/}
+        <li><a href="#" tabIndex="1">Home</a></li>
+        <li><a href="#artview" tabIndex="2">ArtView</a></li>
+        <li><a href="#search" tabIndex="3">Search</a></li>
+       <li><a href="#tourview" tabIndex="4">TourView</a></li>
+       <li><a href="#videotour" tabIndex="5">VideoTour</a></li>
     </ul>
 
 </nav>
