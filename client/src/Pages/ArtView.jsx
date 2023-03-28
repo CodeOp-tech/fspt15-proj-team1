e833a064834c0a5a0510a37319deae9c1498647d
Rebecca Asuan-O'Brien <raaob78@gmail.com>
2023-03-28 22:44:57 +0200

    DRAFT TOURVIEW

--- client/src/Pages/ArtView.jsx
+++ client/src/Pages/ArtView.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
+
+const API_Key = "IagrCxtB";
 
 function ArtView() {
-  return <div>ArtView</div>;
+  let params = useParams();
+  const [artwork, setArtwork] = useState({});
+
+  const fetchArtwork = async () => {
+
+      const resp = await fetch(
+      // API ROUTE HERE
+      `https://www.rijksmuseum.nl/api/en/collection?key=${API_Key}&hasImage=true&p=10.000&ps=100`
+    );
+    
+    const data = await resp.json();
+    return data;
+
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+
+    artView().then((data) => {
+      if (isMounted) setArtwork(data);
+    });
+    return () => {
+      isMounted = false;
+    };
+  }, // from API [params.id]);
+
+
+
+
+
+// WILL NEED TO REVISE TO COORDINATE WITH TOURVIEW AND API INFO
+// ArtView USES collection, setCollection
+// TourView USES artwork, setArtwork
+// LOOK AT API DATA TO MATCH NAMES
+// NOTE principalOrFirstMaker SEEMS TO BE ARTIST NAME
+// DON'T SEE DESCRIPTION OPTION IN API??
+
+  return (
+    <div className="ArtView">
+      <img src={artwork.artObjects[0].webImage.url} alt={artwork.title} />
+      <h2>{artwork.artObjects[0].title}</h2>
+        <div>{artwork.artObjects[0].principalOrFirstMaker}</div>
+        <div>{artwork.artObjects[0].description}</div>
+        <div>{artwork.artObjects[0].type}</div>
+        <div>{artwork.artObjects[0].material}</div>
+        <div>{artwork.artObjects[0].technique}</div>
+    </div>;
+  )
 }
 
 export default ArtView;
