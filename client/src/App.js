7655c9c51400ce92dd33b995f44b131dd3d7465b
Rebecca Asuan-O'Brien <raaob78@gmail.com>
2023-03-31 09:52:26 +0200

    DEBUGGING STILL IN PROGRESS

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,28 @@
 import Search from "./Pages/Search";
 import TourView from "./Pages/TourView";
 import ArtView from "./Pages/ArtView";
-import NavBar from "./Components/NavBar";
-import Footer from "./Components/Footer";
+//import NavBar from "./Components/NavBar";
+//import Footer from "./Components/Footer";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import VideoTour from "./Pages/VideoTour";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    /*<div className="App">
       <NavBar />
       <Search />
       <TourView />
       <ArtView />
-      <Footer />
-    </div>
+      <Footer />*/
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/TourView" element={<TourView />} />
+      <Route path="/VideoTour" element={<VideoTour />} />
+    </Routes>
+    //</div>
   );
 }
 
