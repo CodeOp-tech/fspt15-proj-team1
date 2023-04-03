4eb1813263f517063beec266825327066ca5051b
GabiSza <108346098+GabiSza@users.noreply.github.com>
2023-04-03 12:40:20 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -16,13 +16,16 @@ function App() {
 
 			<TourView />
 			<ArtView /> */}
-			<Footer />
+			
 
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/TourView" element={<TourView />} />
-				<Route path="/VideoTour" element={<VideoTour />} />
-			</Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/TourView" element={<TourView />} />
+        <Route path="/ArtView/:objectNumber" element={<ArtView />} />
+        <Route path="/VideoTour" element={<VideoTour />} />
+      </Routes>
+
+<Footer />
 		</div>
 	);
 }
