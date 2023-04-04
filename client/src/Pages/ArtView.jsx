da9ea93e74fc2262b10d77b8f9a14995cc3b0e3b
Gabi <gabyszabo5@gmail.com>
2023-04-04 19:07:42 +0200

    added more comments to ArtView

--- client/src/Pages/ArtView.jsx
+++ client/src/Pages/ArtView.jsx
@@ -2,13 +2,18 @@ import { useEffect, useState, useCallback } from "react";
 import { useParams, Link } from "react-router-dom";
 
 function ArtView() {
+  // Get the objectNumber from the URL parameter using useParams() hook
   let params = useParams();
   console.log("Object number:", params.objectNumber);
+
+  // Set up state variables for the artwork and whether it is currently being read out loud
   const [artwork, setArtwork] = useState({});
   const [isReading, setIsReading] = useState(false);
 
+  // API key for the Rijksmuseum API
   const API_Key = "IagrCxtB";
 
+  // Define a function to fetch the artwork data from the API
   const fetchArtwork = useCallback(async () => {
     try {
       const resp = await fetch(
@@ -23,19 +28,23 @@ function ArtView() {
     }
   }, [API_Key, params.objectNumber]);
 
+  // Use the useEffect() hook to fetch the artwork data when the component mounts
   useEffect(() => {
     let isMounted = true;
-
     fetchArtwork().then((data) => {
+      // Set the artwork state variable only if the component is still mounted
       if (isMounted) setArtwork(data);
     });
     return () => {
+      // Clean up function to set isMounted to false when the component unmounts
       isMounted = false;
     };
   }, [fetchArtwork]);
 
+  // Define a function to toggle whether the description is being read out loud
   const toggleReading = () => {
     if (!isReading) {
+      // Create a new SpeechSynthesisUtterance object and speak the description
       const speech = new SpeechSynthesisUtterance(
         artwork.artObject.label.description
       );
@@ -44,22 +53,27 @@ function ArtView() {
       speech.onend = () => setIsReading(false);
       window.speechSynthesis.speak(speech);
     } else {
+      // Stop speaking if the description is already being read out loud
       window.speechSynthesis.cancel();
       setIsReading(false);
     }
   };
 
   return (
+    // Render the artwork data if it has been fetched
     <div className="ArtView" style={{ maxWidth: "80%", margin: "auto" }}>
       {artwork.artObject && (
         <>
+          {/* Render the artwork image */}
           <img
             src={artwork.artObject.webImage.url}
             alt={artwork.artObject.title}
             style={{ maxWidth: "50%", height: "auto" }}
           />
+          {/* Render the artwork title */}
           <h2 style={{ margin: "20px 0" }}> {artwork.artObject.longTitle}</h2>
 
+          {/* Render the artwork scLabelLine */}
           <h4
             style={{
               border: "1px solid black",
@@ -70,6 +84,7 @@ function ArtView() {
             {artwork.artObject.scLabelLine}
           </h4>
 
+          {/* Render the artwork description and a button to toggle reading it out loud */}
           <div
             style={{
               border: "1px solid black",
@@ -79,7 +94,8 @@ function ArtView() {
             <h6 style={{ fontSize: "30px" }}>
               {artwork.artObject.label.description}
             </h6>
-            <button style={{fontSize: "20px", fontWeight: "bold", margin: "10px 0"}} onClick={toggleReading}>
+            <button style={{fontSize: "20px",
+ fontWeight: "bold", margin: "10px 0"}} onClick={toggleReading}>
               {isReading ? "Stop Reading" : "Read Description"}
             </button>
           </div>
