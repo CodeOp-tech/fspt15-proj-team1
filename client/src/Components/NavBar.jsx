8c9b05d20a592b98c347ebcdac0ec8aeea78349a
Tatiana <tatiana.motterle@gmail.com>
2023-04-05 11:10:58 +0100

    Gallery: Load more button added; New See gallery button

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,40 +1,47 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
-import { FiHome, FiVideo, FiImage } from "react-icons/fi";
+import { FiHome, FiVideo, FiImage } from 'react-icons/fi';
+
 import image from './logo-color.jpg';
 
 function NavBar() {
-  return (
-    <nav>
-  <ul className="navbar">
-    <li>
-      <NavLink to="/" className="nav-link">
-        <FiHome className="nav-icon" />
-        <span className="nav-text">Home</span>
-      </NavLink>
-    </li>
-    <li>
-      <NavLink to="/TourView" className="nav-link">
-        <FiImage className="nav-icon" />
-        <span className="nav-text">Gallery</span>
-      </NavLink>
-    </li>
-    <li>
-      <NavLink to="/VideoTour" className="nav-link">
-        <FiVideo className="nav-icon" />
-        <span className="nav-text">VideoTour</span>
-      </NavLink>
-    </li>
-    <li className="logo">
-      <img  src={image} style={{ width: "250px", height: "250px" }}/>
-    </li>
-  </ul>
-</nav>
+	/* const showButton = () => {
+		setHidden(false);
+	}; */
 
-  );
+	//const showButton = useContext(BtnContext);
+	return (
+		<nav>
+			<ul className="navbar">
+				<li>
+					<NavLink to="/" className="nav-link">
+						<FiHome className="nav-icon" />
+						<span className="nav-text">Home</span>
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/TourView" className="nav-link">
+						<FiImage className="nav-icon" />
+						<span className="nav-text">Gallery</span>
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/VideoTour" className="nav-link">
+						<FiVideo className="nav-icon" />
+						<span className="nav-text">VideoTour</span>
+					</NavLink>
+				</li>
+				<li className="logo">
+					<img
+						alt="logo"
+						src={image}
+						style={{ width: '250px', height: '250px' }}
+					/>
+				</li>
+			</ul>
+		</nav>
+	);
 }
-
-
 
 export default NavBar;
--- client/src/Pages/TourView.jsx
+++ client/src/Pages/TourView.jsx
@@ -1,72 +1,115 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import "./TourView.css";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './TourView.css';
+import { FiImage } from 'react-icons/fi';
+import { HiOutlinePaintBrush } from 'react-icons/hi2';
 
-const API_Key = "IagrCxtB";
+import { ImImages, ImEye, ImEyePlus } from 'react-icons/im';
+
+import { TripleMaze } from 'react-spinner-animated';
+
+import 'react-spinner-animated/dist/index.css';
+
+const API_Key = 'IagrCxtB';
 
 //const imgUrl = collection[id].webImage.url;
 
-function TourView() {
-  const [collection, setCollection] = useState();
-  const [error, setError] = useState("");
+function TourView({ hidden, setHidden }) {
+	const [collection, setCollection] = useState();
+	const [error, setError] = useState('');
+	//const [hidden, setHidden] = useState(false);
+	const [loading, setLoading] = useState(false);
 
-  const handleClick = (e) => {
-    e.preventDefault();
+	const handleClick = (e) => {
+		e.preventDefault();
+		getCollection();
+		//	hideButton();
+	};
 
-    getCollection();
-  };
+	/* const hideButton = () => {
+		hidden(true);
+	}; */
 
-  const getCollection = async () => {
-    try {
-      let response = await fetch(
-        `https://www.rijksmuseum.nl/api/en/collection?key=${API_Key}&hasImage=true&p=10.000&ps=100`
-      );
-      if (response.ok) {
-        let data = await response.json();
-        let imagesArray = data.artObjects;
-        setCollection(imagesArray);
-        console.log(data.artObjects);
-      } else {
-        setError(`Error: ${response.status} ${response.statusText}`);
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+	const [rows, setRows] = useState(8);
 
-  return (
-    <div className="TourView">
-      <button
-        type="button"
-        onClick={handleClick}
-        className="btn btn-primary btn-lg btn-block"
-      >
-        START VIRTUAL TOUR
-      </button>
-      <div className="container container-grid">
-        {collection && (
-          <div className="grid">
-            {/*  <h3>{collection[1].title}</h3>*/}
-            {collection.map((obj, index) => (
-              <Link to={`/ArtView/${obj.objectNumber}`} key={obj.objectNumber}>
-                <img
-                  className="coll-img"
-                  alt="description"
-                  src={obj.webImage.url}
-                />
-              </Link>
-            ))}
-          </div>
-          /* <div className="col-sm">
-							<img alt="description" src={collection[0].webImage.url} />
-						</div>
-						<div className="col-sm">
-							<img alt="description" src={collection[0].webImage.url} />
-						</div> */
-        )}
-      </div>
-    </div>
-  );
+	const handleLoadMore = () => {
+		setRows(rows + 8);
+	};
+
+	const getCollection = async () => {
+		try {
+			setLoading(true);
+			let response = await fetch(
+				`https://www.rijksmuseum.nl/api/en/collection?key=${API_Key}&hasImage=true&p=10.000&ps=100`
+			);
+			if (response.ok) {
+				setLoading(false);
+				let data = await response.json();
+				let imagesArray = data.artObjects;
+				setCollection(imagesArray);
+				console.log(data.artObjects);
+			} else {
+				setError(`Error: ${response.status} ${response.statusText}`);
+			}
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
+	//	const visibleImages = collection.slice(0, rows);
+	return (
+		<div className="TourView">
+			{!hidden && (
+				<button type="button" onClick={handleClick} className="btn">
+					<ImEye className="nav-icon" />
+					<br />
+					<span className="nav-text">See Gallery</span>
+				</button>
+			)}
+			{loading ? (
+				<>
+					<TripleMaze center={true} width={'150px'} height={'150px'} />
+					<br />
+					<span className="spinner-text">Loading images</span>
+				</>
+			) : (
+				<div className="container container-grid">
+					{collection && (
+						<>
+							<div className="grid">
+								{/*  <h3>{collection[1].title}</h3>*/}
+								{collection.slice(0, rows).map((obj, index) => (
+									<Link
+										to={`/ArtView/${obj.objectNumber}`}
+										key={obj.objectNumber}>
+										<img
+											className="coll-img"
+											key="index"
+											alt="description"
+											src={obj.webImage.url}
+										/>
+									</Link>
+								))}
+							</div>
+							<div>
+								{rows < collection.length && (
+									<button
+										type="button"
+										onClick={handleLoadMore}
+										className="btn">
+										<ImEyePlus className="nav-icon" />
+
+										<br />
+										<span className="nav-text">Load More</span>
+									</button>
+								)}
+							</div>
+						</>
+					)}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default TourView;
